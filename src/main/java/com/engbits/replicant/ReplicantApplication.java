154b4c8fa25e675f74c8bc07dca9d2cc6594197d
Justin Hanney <justin@hanney.net>
2017-07-14 16:19:57 -0700

    Add properties files to get Spring Boot application to start

--- src/main/java/com/engbits/replicant/ReplicantApplication.java
+++ src/main/java/com/engbits/replicant/ReplicantApplication.java
@@ -1,7 +1,11 @@
 package com.engbits.replicant;
 
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.support.SpringBootServletInitializer;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.context.annotation.PropertySources;
 
 /**
  * Primary entry point for the Replication Spring Boot application
@@ -9,6 +13,12 @@ import org.springframework.boot.web.support.SpringBootServletInitializer;
  * @author replicant.team
  * @since 0.1.0
  */
+@SpringBootApplication
+@Configuration
+@PropertySources({
+        @PropertySource("classpath:replicant.properties"),
+        @PropertySource(value = "file:${external.config}", ignoreResourceNotFound = true),
+})
 public class ReplicantApplication extends SpringBootServletInitializer {
 
     public static void main(String[] args) {

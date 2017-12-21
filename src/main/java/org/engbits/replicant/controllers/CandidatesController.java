681490204e3987b83ab5574fb9d7071adab812a4
Justin Hanney <justin@hanney.net>
2017-12-21 14:59:14 -0800

    Show actual data from CandidatesService in template

--- src/main/java/org/engbits/replicant/controllers/CandidatesController.java
+++ src/main/java/org/engbits/replicant/controllers/CandidatesController.java
@@ -4,11 +4,15 @@
 
 package org.engbits.replicant.controllers;
 
+import org.engbits.replicant.model.Candidate;
 import org.engbits.replicant.service.CandidatesService;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import javax.inject.Inject;
+
+import java.util.List;
 
 /**
  * Controller for Candidates functionality
@@ -27,7 +31,10 @@ public class CandidatesController {
     }
 
     @RequestMapping("/candidates")
-    public String showCandidates() {
+    public String showCandidates(final Model model) {
+        final List<Candidate> candidates = candidatesService.getCandidates();
+        model.addAttribute("candidates", candidates);
+
         return "candidates";
     }
 }

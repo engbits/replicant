b0e4d951dff2c4e3e8aafd7d4b8d8fe91b37dcb0
Justin Hanney <justin@hanney.net>
2017-12-28 09:18:54 -0800

    Switch Controllers to RequestMapping

--- src/main/java/org/engbits/replicant/controllers/CandidatesController.java
+++ src/main/java/org/engbits/replicant/controllers/CandidatesController.java
@@ -8,9 +8,9 @@ import org.engbits.replicant.model.Candidate;
 import org.engbits.replicant.service.CandidatesService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
 import javax.inject.Inject;
 
@@ -23,23 +23,24 @@ import java.util.List;
  * @since 0.1.0
  */
 @Controller
+@RequestMapping("/candidates")
 public class CandidatesController {
 
-    private CandidatesService candidatesService;
+    private final CandidatesService candidatesService;
 
     @Inject
     public CandidatesController(final CandidatesService candidatesService) {
         this.candidatesService = candidatesService;
     }
 
-    @PostMapping("/candidates")
+    @RequestMapping(method = RequestMethod.POST)
     public String addCandidate(@ModelAttribute final Candidate candidate) {
         candidatesService.createCandidate(candidate);
 
         return "redirect:/candidates";
     }
 
-    @GetMapping("/candidates")
+    @RequestMapping(method = RequestMethod.GET)
     public String showCandidates(final Model model) {
         final List<Candidate> candidates = candidatesService.getCandidates();
         model.addAttribute("candidates", candidates);
@@ -47,7 +48,7 @@ public class CandidatesController {
         return "candidates";
     }
 
-    @GetMapping("/candidates/add")
+    @RequestMapping(value = "/add", method = RequestMethod.GET)
     public String showAddCandidate(final Model model) {
         model.addAttribute("candidate", new Candidate());
 
--- src/main/java/org/engbits/replicant/controllers/JobsController.java
+++ src/main/java/org/engbits/replicant/controllers/JobsController.java
@@ -7,11 +7,11 @@ package org.engbits.replicant.controllers;
 import org.engbits.replicant.model.Job;
 import org.engbits.replicant.service.JobsService;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.ModelAndView;
 
 import javax.inject.Inject;
 
@@ -24,43 +24,49 @@ import java.util.List;
  * @since 0.1.0
  */
 @Controller
+@RequestMapping("/jobs")
 public class JobsController {
 
-    private JobsService jobsService;
+    private final JobsService jobsService;
 
     @Inject
     public JobsController(final JobsService jobsService) {
         this.jobsService = jobsService;
     }
 
-    @PostMapping("/jobs")
+    @RequestMapping(method = RequestMethod.POST)
     public String addJob(@ModelAttribute final Job job) {
         jobsService.createJob(job);
 
         return "redirect:/jobs";
     }
 
-    @GetMapping("/jobs/{jobId}")
-    public String showJob(final Model model, @PathVariable("jobId") final Long jobId) {
+    @RequestMapping(value = "/{jobId}", method = RequestMethod.GET)
+    public ModelAndView showJob(@PathVariable("jobId") final Long jobId) {
         final Job job = jobsService.getJobById(jobId);
-        model.addAttribute("job", job);
 
-        return "job_detail";
+        final ModelAndView mv = new ModelAndView("job_detail");
+        mv.addObject("job", job);
+
+        return mv;
     }
 
-    @GetMapping("/jobs")
-    public String showJobs(final Model model) {
+    @RequestMapping(method = RequestMethod.GET)
+    public ModelAndView showJobs() {
         final List<Job> jobs = jobsService.getJobs();
-        model.addAttribute("jobs", jobs);
 
-        return "jobs";
+        final ModelAndView mv = new ModelAndView("jobs");
+        mv.addObject("jobs", jobs);
+
+        return mv;
     }
 
-    @GetMapping("/jobs/add")
-    public String showAddJob(final Model model) {
-        model.addAttribute("job", new Job());
+    @RequestMapping(value = "/add", method = RequestMethod.GET)
+    public ModelAndView showAddJob() {
+        final ModelAndView mv = new ModelAndView("jobs");
+        mv.addObject("job", new Job());
 
-        return "add_job";
+        return mv;
     }
 
 }
